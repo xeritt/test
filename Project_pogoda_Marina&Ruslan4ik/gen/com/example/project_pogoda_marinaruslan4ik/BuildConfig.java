ffc79a1dc400f5f75d81cb6b9e63d3f2c5eb6785
user2 <ruslan4ik>
2013-09-10 19:08:07 +0700

    The first commitIK

---
+++ Project_pogoda_Marina&Ruslan4ik/gen/com/example/project_pogoda_marinaruslan4ik/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.project_pogoda_marinaruslan4ik;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
